a8861e54df22ac358fa41ab35826b6d14d6fcfe7
Fortune Linus <84579763+Dfortune014@users.noreply.github.com>
2024-07-17 21:24:07 -0500

    correction

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,7 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  server: {
-    port: Number(process.env.PORT) || 5173,
-    host: '0.0.0.0',
-  },
+  base: '/MERN/', // Make sure this matches your repository name
   build: {
     outDir: 'dist',
   },
